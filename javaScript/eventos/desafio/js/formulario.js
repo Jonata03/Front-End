b7af72040dacde25690d43414193adfec342e35e
Jonata03 <jonatanery2016@gmail.com>
2024-04-03 15:02:13 -0400

    salvar dados em local storage

--- javaScript/eventos/desafio/js/formulario.js
+++ javaScript/eventos/desafio/js/formulario.js
@@ -4,48 +4,49 @@ const btnFecharFeedback = feedbackMessage.querySelector("button")
 const feedBackTexto = feedbackMessage.querySelector("p")
 const caracteresDescricao = document.getElementById("txtDescricao")
 const totalLetras = document.querySelector("#contador span")
+const titulo = document.getElementById("txtTitulo")
+const descricao = document.getElementById("txtDescricao")
+const formCadastro = document.querySelector(".formCadastro")
+const check = document.querySelector("input[type='checkbox']")
 
-function quantidadeCaractere(){
-    totalLetras.innerHTML = (parseInt(caracteresDescricao.maxLength) - parseInt(caracteresDescricao.value.length))
-}
-function salvar(){
-    const titulo = document.getElementById("txtTitulo")
+formCadastro.addEventListener("submit",function(e){
     let txtTitulo = titulo.value
-    const descricao = document.getElementById("txtDescricao")
     let txtDescricao = descricao.value
     if(txtDescricao === "" && txtTitulo === ""){
+        e.preventDefault()
+        titulo.focus()
         feedback(7)
         feedbackTxt("Adicione um titulo e uma descrição")
     }
     else if (txtDescricao === ""){
+        e.preventDefault()
+        descricao.focus()
         feedback(7)
         feedbackTxt("Por gentileza adicione uma descrição")
     }
     else if(txtTitulo === ""){
+        e.preventDefault()
         feedback(7)
         feedbackTxt("Coloque o titulo")
     }
     else{
         feedback(1)
     }
-}
-caracteresDescricao.addEventListener("keypress",quantidadeCaractere)
-caracteresDescricao.addEventListener("keydown",quantidadeCaractere)
-btnAdicionar.addEventListener("keyup", salvar)
-
-btnAdicionar.addEventListener("click",function(e){
-    e.preventDefault()
-    salvar()
 })
-const check = document.querySelector("input[type='checkbox']")
+caracteresDescricao.addEventListener("input",quantidadeCaractere)
+
+// caracteresDescricao.addEventListener("keydown",quantidadeCaractere)
 check.addEventListener("change",function(){
     console.log(check.checked)
     btnAdicionar.disabled = !this.checked
 })
-
 btnFecharFeedback.addEventListener("click",function(){
     feedback(1)
 })
+
+function quantidadeCaractere(){
+    totalLetras.innerHTML = (parseInt(caracteresDescricao.maxLength) - parseInt(caracteresDescricao.value.length))
+}
 function feedbackTxt(texto){
     feedBackTexto.innerText = texto
 }
